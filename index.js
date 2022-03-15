365d4f53feb7f8a9162c33e3de6c0831369408a6
foghill <ben@thefoghill.com>
2022-03-15 14:45:53 -0400

    commit tree 524c88c91889435f56d5d0816b4fc9bd13ca8b5b

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then(function (response) {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+// }
+
+
+function fetchBooks(){
+return fetch('https://anapioficeandfire.com/api/books')
+.then(response => response.json())
+.then (books => {
+  renderBooks(books)
+  console.log(books[4])
 }
+
+)}
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
